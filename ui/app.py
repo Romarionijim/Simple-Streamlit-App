7d78f6b134e99e5a49bcffdbecd449e50d200d3e
Romario Nijim <rn@Romarios-MacBook-Pro.local>
2026-06-29 11:12:07 +0300

    refactor(app.py): cleanup char len check and add submit logic

--- ui/app.py
+++ ui/app.py
@@ -5,20 +5,19 @@ import phonenumbers
 
 st.title('Basketball Camp Regitser From')
 
-name_field = st.text_input("Please enter the full name of the athlete:")
-if name_field:
-    field_len = len(name_field)
-    if field_len < 2 or field_len > 10:
+def check_char_length(char: str):
+    if len(char) < 2 or len(char) > 10:
         st.error("Name is too short or too long - min 2 and max 10 characters")
 
-last_name_field = st.text_input("Please enter your last name:")
+name_field = st.text_input("First name:")
+if name_field:
+    check_char_length(name_field)
+
+last_name_field = st.text_input("Last name:")
 if last_name_field:
-    last_name_len = len(last_name_field)
-    if last_name_len < 2 or last_name_len > 10:
-        st.error("Last name is too short or too long - min 2 and max 10 characters")
+    check_char_length(last_name_field)
 
-
-email_field = st.text_input("Please enter your email address:")
+email_field = st.text_input("Email address:")
 if email_field:
     email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
     if not re.match(email_pattern, email_field):
@@ -26,14 +25,19 @@ if email_field:
     else:
         st.success("Email is valid")
 
+player_age = st.number_input("Age:", min_value=18, max_value=45, value=18)
+
+current_team = st.text_input("Type your current league team:", placeholder="Hapoel Nazareth Orthodox")
+
 col1, col2 = st.columns([1, 3])
 
 with col1:
-    phone_country_code = st.selectbox("Code", ['+1 🇺🇸', '+44 🇬🇧', '+91 🇮🇳', '+972 🇮🇱'], index=3)
+    phone_country_code = st.selectbox("Country Code", ['+1 🇺🇸', '+44 🇬🇧', '+91 🇮🇳', '+972 🇮🇱'], index=3)
 
 with col2:
     phone_number_field = st.text_input("Phone number")
 
+phone_number = None
 if phone_number_field:
     code = phone_country_code.split()[0]
     phone_number = code + phone_number_field
@@ -45,3 +49,21 @@ if phone_number_field:
             st.error("Invalid phone number for the selected country code")
     except phonenumbers.NumberParseException:
         st.error("Could not parse phone number - digits only, no spaces or dashes")
+
+submit_button = st.button("Submit")
+
+if submit_button:
+    payload = {
+        "first_name": name_field,
+        "last_name": last_name_field,
+        "email": email_field,
+        "age": player_age,
+        "current_team": current_team,
+        "phone_number": phone_number
+    }
+
+    response = requests.post("http://localhost:3030/register", json=payload)
+    if response.status_code == 200:
+        st.success("Registration successful")
+    else:
+        st.error("Registration failed")
